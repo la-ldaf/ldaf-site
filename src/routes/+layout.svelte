6c53961e9b9ae4300be8518681adf2a53038cb45
Dan Hinze <hinzed1127@gmail.com>
2023-03-06 20:30:24 -0600

    LDAF-146: LDAF Sveltekit Skeleton (#42)
    * Svelte CLI skeleton files
    
    * Add Svelte adder for SCSS
    
    * Remove duplicative vite config
    
    * Commit this before I break it
    
    * Feels kinda hacky, but got fonts loading for now
    
    * improve uswds style setup, setup global uswds styles
    
    * Cleanup/fix settings a bit more
    
    * landing page 99% good (TODO: fix top accordion)
    
    * Get banner toggle working, add in a primary nav
    
    * Move routes around and clean up
    
    ---------
    
    Co-authored-by: Benaiah Mischenko <benaiah@mischenko.com>

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.scss';
+</script>
+
+<slot />
+
+<style lang="scss">
+	@use 'src/app.scss';
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@use "src/variables.scss" as *;',
+				includePaths: ['./node_modules/@uswds/uswds/packages']
+			}
+		}
+	}
+});
