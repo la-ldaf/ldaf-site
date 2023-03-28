61f824e360121990ac73baea8f33d0f7418a92a2
Louis Fettet <louisfettet@gmail.com>
2023-03-28 17:54:55 -0500

    Set up initial version of <Nav/> component for desktop.

---
+++ src/lib/components/Nav/Nav.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import "./Nav.scss";
+  import type { NavItemType } from "./NavItem.svelte";
+  import NavItem from "./NavItem.svelte";
+
+  export let items: NavItemType[] = [];
+</script>
+
+<nav class="usa-nav">
+  <ul class="usa-nav__primary usa-accordion">
+    {#each items as { id, link, isCurrent, children, name } (id)}
+      <li class="usa-nav__primary-item">
+        <NavItem {id} {link} {isCurrent} {children}>{name}</NavItem>
+      </li>
+    {/each}
+  </ul>
+</nav>
---
+++ src/stories/decorators/HeaderNav.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import "@uswds/uswds/scss/usa-header";
+</script>
+
+<header class="usa-header usa-header--basic">
+  <div class="usa-nav-container">
+    <slot />
+  </div>
+</header>
