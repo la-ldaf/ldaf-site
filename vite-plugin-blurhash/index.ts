95845fbf33f67b2c76f5bea9f3fe79ff3c4871b5
Benaiah Mischenko <benaiah@mischenko.com>
2023-04-03 13:16:14 -0700

    vite-plugin-blurhash: use sharp's async API

--- vite-plugin-blurhash/index.ts
+++ vite-plugin-blurhash/index.ts
@@ -16,55 +16,26 @@ type BlurHashAndInfo = {
   mean: Color;
 };
 
-/* const encodePathToBlurHashAndInfo = async (path: string): Promise<BlurHashAndInfo> => {
- *   const image = sharp(path);
- *   const [
- *     { channels },
- *     {
- *       data: buffer,
- *       info: {
- *         size: { width, height },
- *       },
- *     },
- *   ] = await Promise.all([image.stats(), image.toBuffer({ resolveWithObject: true })]);
- *   console.log({ channels });
- *   const blurhash = encode(new Uint8ClampedArray(buffer), width, height, 4, 4);
- *   return {
- *     width,
- *     height,
- *     blurhash,
- *   };
- * };
- *  */
-const encodePathToBlurHashAndInfoOld = async (path: string): Promise<BlurHashAndInfo> => {
-  const image = sharp(path);
-  const statsPromise = image.stats();
-  const blurhashPromise = new Promise<{ blurhash: string; width: number; height: number }>(
-    (resolve, reject) => {
-      image
-        .raw()
-        .ensureAlpha()
-        .toBuffer((err, buffer, { width, height }) => {
-          if (err) return reject(err);
-          resolve({
-            blurhash: encode(new Uint8ClampedArray(buffer), width, height, 4, 4),
-            width,
-            height,
-          });
-        });
-    }
-  );
-  const [stats, { blurhash, width, height }] = await Promise.all([statsPromise, blurhashPromise]);
-  const {
+const encodePathToBlurHashAndInfo = async (path: string): Promise<BlurHashAndInfo> => {
+  const image = sharp(path).raw().ensureAlpha();
+  const [
+    {
+      dominant,
+      channels: [r, g, b],
+    },
+    {
+      data: buffer,
+      info: { width, height },
+    },
+  ] = await Promise.all([image.stats(), image.toBuffer({ resolveWithObject: true })]);
+  const blurhash = encode(new Uint8ClampedArray(buffer), width, height, 4, 4);
+  return {
+    width,
+    height,
+    blurhash,
     dominant,
-    channels: [r, g, b],
-  } = stats;
-  const mean = {
-    r: r.mean,
-    g: g.mean,
-    b: b.mean,
+    mean: { r: r.mean, g: g.mean, b: b.mean },
   };
-  return { blurhash, width, height, dominant, mean };
 };
 
 export default () => {
@@ -75,10 +46,8 @@ export default () => {
     enforce: "pre",
     load: async (id: string) => {
       if (!id.match(imageRegex)) return;
-      const [path] = id.split("?");
-      const { width, height, blurhash, dominant, mean } = await encodePathToBlurHashAndInfoOld(
-        path
-      );
+      const [path] = id.split("?", 2);
+      const { width, height, blurhash, dominant, mean } = await encodePathToBlurHashAndInfo(path);
       return `\
 export default ${JSON.stringify(blurhash)};
 export const width = ${width};
