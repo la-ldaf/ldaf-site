8a7f914d79ebf3b009aef5cdd8cea16669bfd2a0
Benaiah Mischenko <benaiah@mischenko.com>
2023-04-12 14:28:05 -0700

    remove unnecessary brackets

--- src/lib/util/chunk.ts
+++ src/lib/util/chunk.ts
@@ -1,9 +1,10 @@
 // https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_chunk
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const chunk = (input: any[], size: number) => {
-  return input.reduce((arr, item, idx) => {
-    return idx % size === 0 ? [...arr, [item]] : [...arr.slice(0, -1), [...arr.slice(-1)[0], item]];
-  }, []);
-};
+const chunk = (input: any[], size: number) =>
+  input.reduce(
+    (arr, item, idx) =>
+      idx % size === 0 ? [...arr, [item]] : [...arr.slice(0, -1), [...arr.slice(-1)[0], item]],
+    []
+  );
 
 export default chunk;
