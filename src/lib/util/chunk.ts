f19d510ce7946586847cbad91042a6bee0b4ec98
Louis Fettet <louis@adhocteam.us>
2023-04-13 19:36:35 -0500

    Add an escape hatch back to chunk and add some very simple tests.

--- src/lib/util/chunk.ts
+++ src/lib/util/chunk.ts
@@ -1,10 +1,12 @@
 // https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_chunk
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const chunk = <T>(input: T[], size: number): T[][] =>
-  input.reduce<T[][]>(
+const chunk = <T>(input: T[], size: number): T[][] => {
+  // Eject early if we're attempting to divide up items into arrays of length zero.
+  if (size === 0) return [];
+  return input.reduce<T[][]>(
     (arr, item, i) =>
       i % size === 0 ? [...arr, [item]] : [...arr.slice(0, -1), [...arr.slice(-1)[0], item]],
     []
   );
-
+};
 export default chunk;
