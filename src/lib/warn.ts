523253a76c550ed9b2ad4ad25a6bc1b4a6c8042a
Benaiah Mischenko <benaiah@mischenko.com>
2023-04-17 17:20:56 -0700

    don't warn in tests

--- src/lib/warn.ts
+++ src/lib/warn.ts
@@ -2,5 +2,6 @@ import { browser } from "$app/environment";
 
 export default (...args: Parameters<typeof console.warn>) => {
   if (browser && import.meta.env.MODE === "production") return;
+  if (process?.env?.NODE_ENV === "test") return;
   return console.warn(...args);
 };
