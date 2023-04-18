261cd161117e39a8af133c0b01147ee74ee6e781
Dan Hinze <hinzed1127@gmail.com>
2023-04-17 20:55:50 -0500

    Add background icons to Icon component

--- src/lib/components/Icon/icons.ts
+++ src/lib/components/Icon/icons.ts
@@ -1,4 +1,4 @@
-const modules = import.meta.glob<{ default: string }>(
+const usaIconModules = import.meta.glob<{ default: string }>(
   "/node_modules/@uswds/uswds/dist/img/usa-icons/*.svg",
   {
     as: "dataurl",
@@ -6,8 +6,18 @@ const modules = import.meta.glob<{ default: string }>(
   }
 );
 
+const usaBgIconModules = import.meta.glob<{ default: string }>(
+  "/node_modules/@uswds/uswds/dist/img/usa-icons-bg/*.svg",
+  {
+    as: "dataurl",
+    eager: true,
+  }
+);
+
+const allIconModules = { ...usaIconModules, ...usaBgIconModules };
+
 const icons = Object.fromEntries(
-  Object.entries(modules).map(([path, icon]) => [
+  Object.entries(allIconModules).map(([path, icon]) => [
     // Grabs the last item in the array, which is the icon filename + svg extension
     path.split("/").slice(-1)[0].split(".")[0],
     icon,
