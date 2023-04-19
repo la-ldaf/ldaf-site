0334be24e90e0b78556c2c7cd99cda5089dca6b6
Dan Hinze <hinzed1127@gmail.com>
2023-04-19 16:35:45 -0500

    Fix issues with small screen widths

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -23,28 +23,6 @@
 <header class="ldaf-header usa-header usa-header--extended">
   <div class="ldaf-nav usa-navbar">
     <Title />
-    <!-- TODO: Extend <Nav/> to cover secondary nav or build out component with shared dependencies. -->
-    <div class="usa-nav__secondary">
-      <ul class="usa-nav__secondary-links">
-        <li class="usa-nav__secondary-item">
-          <a href="/news">Grants and funding</a>
-        </li>
-        <li class="usa-nav__secondary-item">
-          <a href="/documentation" class="usa-current">Licensing and permits</a>
-        </li>
-      </ul>
-      <!-- TODO: Replace with <Search/> component. -->
-      <section aria-label="Search component">
-        <form class="usa-search usa-search--small " role="search">
-          <label class="usa-sr-only" for="search-field"> Search </label>
-          <input class="usa-input" id="search-field" type="search" name="search" />
-          <button class="usa-button" type="submit">
-            <img src={iconBgSearchWhite} class="usa-search__submit-icon" alt="Search" />
-          </button>
-        </form>
-      </section>
-    </div>
-
     <!--TODO: Replace with content from CMS. -->
     <button type="button" class="usa-menu-btn" on:click={() => toggleNavMenu(true)}> Menu </button>
   </div>
@@ -57,6 +35,29 @@
       </button>
 
       <Nav items={navItems} />
+
+      <!-- TODO: Extend <Nav/> to cover secondary nav or build out component with shared dependencies. -->
+      <div class="ldaf-nav--secondary usa-nav__secondary">
+        <ul class="usa-nav__secondary-links">
+          <li class="usa-nav__secondary-item">
+            <a href="/news">Grants and funding</a>
+          </li>
+          <li class="usa-nav__secondary-item">
+            <a href="/documentation" class="usa-current">Licensing and permits</a>
+          </li>
+        </ul>
+
+        <!-- TODO: Replace with <Search/> component. -->
+        <section aria-label="Search component">
+          <form class="usa-search usa-search--small " role="search">
+            <label class="usa-sr-only" for="search-field"> Search </label>
+            <input class="usa-input" id="search-field" type="search" name="search" />
+            <button class="usa-button" type="submit">
+              <img src={iconBgSearchWhite} class="usa-search__submit-icon" alt="Search" />
+            </button>
+          </form>
+        </section>
+      </div>
     </div>
   </nav>
 </header>
