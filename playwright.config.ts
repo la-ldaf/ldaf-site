ffe1e076adde1d4be4790c42b041dc6a976efe04
Louis Fettet <louis@adhocteam.us>
2023-04-21 16:40:59 -0500

    Add timeout to tests so preview has enough time to spin up.

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,14 +2,14 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   testDir: "e2e-tests",
+  timeout: 60 * 60 * 1000,
   // retry once in CI
   retries: process.env.CI ? 1 : 0,
   // limit number of concurrent workers in CI, use default locally
   workers: process.env.CI ? 2 : undefined,
-  // Use the GitHub Actions reporter for CI; otherwise use the default list reporter and an HTML
-  //   reporter, which can be viewed with `npm run test:e2e:report`.
+  // Use the GitHub Actions reporter for CI; otherwise use the default list reporter.
   reporter: process.env.CI
-    ? "github"
+    ? [["github"], ["html", { outputFolder: "e2e-tests/report", open: "never" }]]
     : [["list"], ["html", { outputFolder: "e2e-tests/report", open: "on-failure" }]],
   use: {
     trace: "on-first-retry",
