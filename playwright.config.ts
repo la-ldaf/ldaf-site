969a5542c2b6f814f9c86122bbb5df1f30535178
Louis Fettet <louis@adhocteam.us>
2023-04-21 13:21:41 -0500

    Update playwright configurations for both CI and local development. Add support for new UI mode. Handle outputting reports and results.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,25 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
+  testDir: "e2e-tests",
+  // retry once in CI
+  retries: process.env.CI ? 1 : 0,
+  // limit number of concurrent workers in CI, use default locally
+  workers: process.env.CI ? 2 : undefined,
+  // Use the GitHub Actions reporter for CI; otherwise use the default list reporter and an HTML
+  //   reporter, which can be viewed with `npm run test:e2e:report`.
+  reporter: process.env.CI
+    ? "github"
+    : [["list"], ["html", { outputFolder: "e2e-tests/report", open: "on-failure" }]],
+  use: {
+    trace: "on-first-retry",
+  },
   webServer: {
     command: "npm run build && npm run preview",
     port: 4173,
+    reuseExistingServer: !process.env.CI,
   },
-  testDir: "tests",
+  outputDir: "e2e-tests/results",
 };
 
 export default config;
