520bfcc949d68e51d514f5f7fdbe05af93eff35c
Benaiah Mischenko <benaiah@mischenko.com>
2023-04-21 16:42:47 -0700

    fix type error

--- src/stories/IntersectionObserver.stories.ts
+++ src/stories/IntersectionObserver.stories.ts
@@ -1,13 +1,13 @@
 import type { Meta, StoryObj } from "@storybook/svelte";
 
-import type IntersectionObserver from "$lib/components/IntersectionObserver";
 import IntersectionObserverStory from "./IntersectionObserverStory.svelte";
 
 const meta = {
   title: "IntersectionObserver",
   component: IntersectionObserverStory,
   tags: [],
-} satisfies Meta<IntersectionObserver>;
+  argTypes: {},
+} satisfies Meta<IntersectionObserverStory>;
 
 type Story = StoryObj<typeof meta>;
 
