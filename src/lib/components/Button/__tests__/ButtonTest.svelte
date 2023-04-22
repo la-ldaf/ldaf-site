696f04479e13bd36b85ad32c662ec5e14d948419
Benaiah Mischenko <benaiah@mischenko.com>
2023-04-21 19:25:20 -0700

    Button: fix import

--- src/lib/components/Button/__tests__/ButtonTest.svelte
+++ src/lib/components/Button/__tests__/ButtonTest.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { ComponentProps } from "svelte";
-  import Button from "./Button.svelte";
+  import Button from "../Button.svelte";
 
   type $$Props = ComponentProps<Button> & {
     slot?: string;
