19840c18c4e06076bdb7f90bf057eecc7879d442
Benaiah Mischenko <benaiah@mischenko.com>
2023-04-25 15:31:57 -0700

    Icon: fix stories

--- src/stories/Icon/IconStory.svelte
+++ src/stories/Icon/IconStory.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
   import Icon from "$lib/components/Icon";
-  import { namesToPaths } from "$icons";
+  import { getIconSrcByName } from "./icons";
 
   export let name: string;
 
   const setSrc = async (name: string) => {
-    const pathWithinImg = namesToPaths[name];
-    const request = await fetch(`./node_modules/@uswds/uswds/dist/img/${pathWithinImg}`);
-    const rawSrc = await request.text();
+    const rawSrc = await getIconSrcByName(name);
     const onelineSrc = rawSrc.replaceAll(/\s+/g, " ");
     const escapedSrc = onelineSrc.replaceAll("#", "%23");
     const url = `data:image/svg+xml;charset=utf8,${escapedSrc}`;
