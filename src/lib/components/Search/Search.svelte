70e94c9ac606d597cee957dae5f27d2748e991ec
Benaiah Mischenko <benaiah@mischenko.com>
2023-04-25 14:16:20 -0700

    Add Search component

---
+++ src/lib/components/Search/Search.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  // import searchIcon from "$icons/search--white.svg";
+  import searchIcon from "@uswds/uswds/img/usa-icons-bg/search--white.svg?url";
+  import Button from "$lib/components/Button";
+  import classNames from "$lib/util/classNames";
+  import type { Size } from "./options";
+
+  export let size: Size = "default";
+
+  export let id: string;
+  export let label = "Search";
+  export let disabled = false;
+  export let inputName = "searchTerm";
+
+  let searchTerm = "";
+
+  type SearchEvent = { searchTerm: string };
+
+  const dispatch = createEventDispatcher<{
+    input: SearchEvent;
+    submit: SearchEvent;
+  }>();
+
+  const onInput = () => dispatch("input", { searchTerm });
+  const onSubmit = (e: Event) => {
+    e.preventDefault();
+    dispatch("submit", { searchTerm });
+  };
+</script>
+
+<form
+  class={classNames(
+    "usa-search",
+    { default: "", small: "usa-search--small", big: "usa-search--big" }[size]
+  )}
+  role="search"
+  on:submit={onSubmit}
+>
+  <label class="usa-sr-only" for={`${id}--input`}>{label}</label>
+  <input
+    class="usa-input"
+    name={inputName}
+    id={`${id}--input`}
+    type="search"
+    bind:value={searchTerm}
+    on:input={onInput}
+    {disabled}
+    aria-disabled={disabled}
+  />
+  <Button type="submit" class="usa-search__submit" {disabled}>
+    {#if size !== "small"}<span class="usa-search__submit-text">{label}</span>{/if}<img
+      src={searchIcon}
+      class="usa-search__submit-icon"
+      alt={label}
+    />
+  </Button>
+</form>
---
+++ src/lib/components/Search/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./Search.svelte";
+export { sizes } from "./options";
