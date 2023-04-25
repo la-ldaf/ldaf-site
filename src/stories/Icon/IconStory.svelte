2576d3ef9e40528b736aef040a9e15671a61e3a1
Benaiah Mischenko <benaiah@mischenko.com>
2023-04-25 16:44:40 -0700

    Icon: type IconStory properly

--- src/stories/Icon/IconStory.svelte
+++ src/stories/Icon/IconStory.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import Icon from "$lib/components/Icon";
+  import type { ComponentProps } from "svelte";
   import { getIconSrcByName } from "./icons";
+
+  type $$Props = Omit<ComponentProps<Icon>, "src"> & { name: string };
 
   export let name: string;
 
