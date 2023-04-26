8a806c886e0c72dbea9084eb7c14a16c3a667ade
Louis Fettet <louis@adhocteam.us>
2023-04-26 17:05:24 -0500

    LDAF-237 Improve end-to-end testing and run the tests in CI (#59)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,26 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
+  testDir: "e2e-tests",
+  // retry once in CI
+  retries: process.env.CI ? 1 : 0,
+  // limit number of concurrent workers in CI, use default locally
+  workers: process.env.CI ? 2 : undefined,
+  // Use the GitHub Actions reporter for CI; otherwise use the default list reporter.
+  reporter: process.env.CI
+    ? [["github"], ["html", { outputFolder: "e2e-tests/report", open: "never" }]]
+    : [["list"], ["html", { outputFolder: "e2e-tests/report", open: "on-failure" }]],
+  use: {
+    trace: "on-first-retry",
+  },
   webServer: {
     command: "npm run build && npm run preview",
     port: 4173,
+    reuseExistingServer: !process.env.CI,
+    // Set a ridiculously long timeout to allow the site to be built.
+    timeout: 60 * 60 * 1000,
   },
-  testDir: "tests",
+  outputDir: "e2e-tests/results",
 };
 
 export default config;
