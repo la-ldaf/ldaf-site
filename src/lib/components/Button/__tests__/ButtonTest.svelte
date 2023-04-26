b8c138cc0554fc284260239fc6ed404a9088fdda
Benaiah Mischenko <benaiah.mischenko@adhocteam.us>
2023-04-26 14:20:52 -0700

    Add Button component (#63)

---
+++ src/lib/components/Button/__tests__/ButtonTest.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { ComponentProps } from "svelte";
+  import Button from "../Button.svelte";
+
+  type $$Props = ComponentProps<Button> & {
+    slot?: string;
+  };
+
+  export let slot: string | undefined = "Button";
+</script>
+
+<Button {...$$restProps} on:click>
+  {slot}
+</Button>
