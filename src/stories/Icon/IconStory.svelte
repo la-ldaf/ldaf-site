db5989cba19a0de6498e625f0d4d5956a36a91af
Benaiah Mischenko <benaiah@mischenko.com>
2023-04-26 12:31:55 -0700

    Icon: fix type error in story by not typing component props

--- src/stories/Icon/IconStory.svelte
+++ src/stories/Icon/IconStory.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   import Icon from "$lib/components/Icon";
-  import type { ComponentProps } from "svelte";
   import { getIconSrcByName } from "./icons";
-
-  type $$Props = Omit<ComponentProps<Icon>, "src"> & { name: string };
 
   export let name: string;
 
