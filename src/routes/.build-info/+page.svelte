f1cfdc09469e41d292f58b2873f739d1049d84e9
Louis Fettet <louis@adhocteam.us>
2023-04-28 11:13:32 -0500

    Link to coverage report from Vercel.

--- src/routes/.build-info/+page.svelte
+++ src/routes/.build-info/+page.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
   import Link from "$lib/components/Link";
   export let data;
-  const { VERCEL_ENV, VERCEL_GIT_REPO_OWNER, VERCEL_GIT_REPO_SLUG, VERCEL_GIT_COMMIT_SHA } = data;
-  const githubUrl = `https://github.com/${VERCEL_GIT_REPO_OWNER}/${VERCEL_GIT_REPO_SLUG}/commit/${VERCEL_GIT_COMMIT_SHA}`;
+  const {
+    VERCEL_ENV,
+    VERCEL_GIT_REPO_OWNER,
+    VERCEL_GIT_REPO_SLUG,
+    VERCEL_GIT_COMMIT_SHA,
+    VERCEL_GIT_PULL_REQUEST_ID,
+  } = data;
+
+  const githubBaseUrl = `https://github.com/${VERCEL_GIT_REPO_OWNER}/${VERCEL_GIT_REPO_SLUG}`;
+  const githubCommitUrl = `${githubBaseUrl}/commit/${VERCEL_GIT_COMMIT_SHA}`;
+  const githubPullUrl = `${githubBaseUrl}/pull/${VERCEL_GIT_PULL_REQUEST_ID}`;
+
+  const githubPagesBaseUrl = `https://${VERCEL_GIT_REPO_OWNER}.github.io/${VERCEL_GIT_REPO_SLUG}`;
+  const coverageReportUrl = `${githubPagesBaseUrl}/pull/${VERCEL_GIT_PULL_REQUEST_ID}/unit-test-coverage`;
 </script>
 
 <section class="usa-section">
@@ -18,9 +30,18 @@
       {#if VERCEL_GIT_REPO_OWNER && VERCEL_GIT_REPO_SLUG && VERCEL_GIT_COMMIT_SHA}
         Build commit SHA: <code>{VERCEL_GIT_COMMIT_SHA}</code>
         <br />
-        Link to commit in GitHub: <Link href={githubUrl}>{githubUrl}</Link>
+        Link to commit in GitHub: <Link href={githubCommitUrl}>{githubCommitUrl}</Link>
       {:else}
         Could not output git information.
+      {/if}
+    </p>
+    <p>
+      {#if VERCEL_GIT_PULL_REQUEST_ID}
+        Link to PR in GitHub: <Link href={githubPullUrl}>{githubPullUrl}</Link>
+        <br />
+        Link to coverage report: <Link href={coverageReportUrl}>{coverageReportUrl}</Link>
+      {:else}
+        This preview is not associated with a pull request.
       {/if}
     </p>
   </div>
