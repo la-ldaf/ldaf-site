b870b7a9b5b699ecd35d3b419c4142aa9ce81c16
Benaiah Mischenko <benaiah@mischenko.com>
2023-05-04 18:39:45 -0700

    generate test cases on build, because they're used for a test page

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,4 +1,9 @@
 import { stat, copyFile } from "fs/promises";
+import runScript from "./runScript.js";
+
+await runScript(
+  "src/lib/components/ContentfulRichText/__tests__/documents/.generate-documents-from-markdown.js"
+);
 
 if (process.env.VERCEL === "1") {
   process.exit(0);
--- scripts/pretest.js
+++ scripts/pretest.js
@@ -1,13 +1,4 @@
-import path from "path";
-import { fork } from "child_process";
-
-const runScript = async (relativeScriptPath) => {
-  await new Promise((resolve, reject) => {
-    const childProcess = fork(path.join(path.resolve("."), relativeScriptPath));
-    childProcess.on("close", resolve);
-    childProcess.on("error", (err) => reject(err));
-  });
-};
+import runScript from "./runScript.js";
 
 await runScript(
   "src/lib/components/ContentfulRichText/__tests__/documents/.generate-documents-from-markdown.js"
