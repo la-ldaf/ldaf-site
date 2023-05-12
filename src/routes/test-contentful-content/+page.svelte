60c8dd668b106e6347b6b9ded3670461d1361fa0
Benaiah Mischenko <benaiah@mischenko.com>
2023-05-12 14:49:46 -0700

    load preview entry if logged in

--- src/routes/test-contentful-content/+page.svelte
+++ src/routes/test-contentful-content/+page.svelte
@@ -1,10 +1,43 @@
 <script lang="ts">
   import type { PageData } from "./$types";
+  import type { TestRichTextEntrySkeleton } from "./types";
   import ContentfulRichText from "$lib/components/ContentfulRichText";
+  import { page } from "$app/stores";
+  import { user } from "$lib/stores";
+  import { getClient } from "$lib/contentful-client";
+  import { browser } from "$app/environment";
 
   export let data: PageData;
+
+  let entry = data.entry;
+  let loading = false;
+
+  const failLoading = (message: string) => {
+    loading = false;
+    console.error(message);
+  };
+
+  const loadPreviewEntry = async () => {
+    loading = true;
+    if (!entry) return failLoading("cannot load preview of entry that doesn't exist");
+    if (!$user) return failLoading("cannot load preview when not logged in");
+    const client = getClient({ token: $user.token, preview: true });
+    if (!client) return failLoading("failed to create contentful preview client");
+    const previewEntry = await client.getEntry<TestRichTextEntrySkeleton>(entry.sys.id);
+    if (!previewEntry) return failLoading("failed to fetch preview entry");
+    loading = false;
+    entry = previewEntry;
+  };
+
+  if (browser && $page.url.searchParams.has("preview")) {
+    if ($user) {
+      loadPreviewEntry();
+    } else {
+      console.error("you must log in to preview the entry!");
+    }
+  }
 </script>
 
-<h1>{data.entry?.fields.title}</h1>
+<h1>{entry?.fields.title}</h1>
 
-<ContentfulRichText document={data.entry?.fields.body} />
+<ContentfulRichText document={entry?.fields.body} />
