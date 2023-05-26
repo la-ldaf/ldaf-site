a2e7d8aad8a0ca01f51f6d60dc3d8099c03905e5
Benaiah Mischenko <benaiah@mischenko.com>
2023-05-25 17:36:22 -0700

    fix oauth callback page

--- src/routes/callback/oauth2/+page.svelte
+++ src/routes/callback/oauth2/+page.svelte
@@ -3,7 +3,7 @@
   import { goto } from "$app/navigation";
   import { page } from "$app/stores";
   import managementClient from "$lib/stores/managementClient";
-  import user from "$lib/stores/user";
+  import userInfo from "$lib/stores/userInfo";
   import parseHashQuery from "$lib/util/parseHashQuery";
   import timeout from "$lib/util/timeout";
 
@@ -22,13 +22,6 @@
     managementClient?.set(createManagementClient({ accessToken }));
     if (!$managementClient) return goto("/", { replaceState: true });
 
-    const { email, firstName, lastName, avatarUrl } = await $managementClient.getCurrentUser();
-    user?.set({
-      email,
-      name: `${firstName} ${lastName}`,
-      avatarURL: avatarUrl,
-    });
-
     if (redirect) {
       await timeout(2500);
       await goto(redirect, { replaceState: true });
@@ -38,11 +31,12 @@
   if (browser) handleHash();
 </script>
 
-{#if $user}
-  <p>Hello {$user.name}!</p>
-  {#if redirect}
-    <p>Please wait to be redirected back to the previous page...</p>
-  {/if}
+{#if $userInfo}
+  <p>Hello {$userInfo.name}!</p>
+{/if}
+
+{#if redirect}
+  <p>Please wait to be redirected back to the previous page...</p>
 {:else}
   <p>Logging in...</p>
 {/if}
