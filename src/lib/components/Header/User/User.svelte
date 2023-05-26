9fafa5e070e1224539dab268581e42ba8a629237
Benaiah Mischenko <benaiah@mischenko.com>
2023-05-26 16:53:49 -0700

    request correct token

--- src/lib/components/Header/User/User.svelte
+++ src/lib/components/Header/User/User.svelte
@@ -16,7 +16,7 @@
   const baseURL = "https://be.contentful.com/oauth/authorize";
   $: loginParams = {
     response_type: "token",
-    scope: "content_management",
+    scope: "content_management_read",
     client_id: PUBLIC_CONTENTFUL_OAUTH_CLIENT_ID,
     redirect_uri: encodeURIComponent(PUBLIC_CONTENTFUL_OAUTH_CLIENT_REDIRECT_URI),
     state: encodeURIComponent($page.url.pathname + $page.url.search + $page.url.hash),
