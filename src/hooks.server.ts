9a3db02a8e601730f5dd233dae8a096354acb1c1
Benaiah Mischenko <benaiah@mischenko.com>
2023-05-26 19:10:41 -0700

    tighten up CSP

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -69,7 +69,7 @@ const handleToken = (async ({ event, resolve }) => {
 
 const handleCSP = (async ({ event, resolve }) => {
   event.setHeaders({
-    "Content-Security-Policy": "frame-ancestors https://*",
+    "Content-Security-Policy": "frame-ancestors 'self' https://app.contentful.com",
   });
   return resolve(event);
 }) satisfies Handle;
