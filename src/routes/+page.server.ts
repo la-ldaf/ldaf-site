0eee88b95fe42db5b1f640d7b39006514eb92d63
Benaiah Mischenko <benaiah@mischenko.com>
2023-06-02 18:20:52 -0700

    convert under construction page to use GraphQL client

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,27 +1,38 @@
-import contentful, { type ContentfulClientApi, type EntryFieldTypes } from "contentful";
-import { contentfulConnected } from "$lib/services/contentful";
-import { homepageTestData } from "$lib/components/ContentfulRichText/__tests__/documents";
 import { CONTENTFUL_SPACE_ID, CONTENTFUL_DELIVERY_API_TOKEN } from "$env/static/private";
+import { print as printQuery } from "graphql";
+import gql from "graphql-tag";
+import getContentfulClient from "$lib/services/contentful";
+import { homepageTestData } from "$lib/components/ContentfulRichText/__tests__/documents";
+import type { EntriesQuery } from "./$queries.generated";
 
-type EntrySkeleton = {
-  contentTypeId: "testRichText";
-  fields: { body: EntryFieldTypes.RichText };
-};
+const query = gql`
+  query Entries {
+    titleEntry: testRichText(id: "5k6saMkIV7PHFhNDxmZiIG") {
+      body {
+        json
+      }
+    }
+    bodyEntry: testRichText(id: "1VCVunA3sIV5Ch1qGHEKZZ") {
+      body {
+        json
+      }
+    }
+  }
+`;
 
 // TODO: refactor to a reusable helper function that wraps contenfulClient
 export async function load() {
-  if (contentfulConnected()) {
-    const contentfulClient: ContentfulClientApi<undefined> = contentful.createClient({
-      space: CONTENTFUL_SPACE_ID,
-      accessToken: CONTENTFUL_DELIVERY_API_TOKEN,
+  if (CONTENTFUL_SPACE_ID && CONTENTFUL_DELIVERY_API_TOKEN) {
+    const client = getContentfulClient({
+      spaceID: CONTENTFUL_SPACE_ID,
+      token: CONTENTFUL_DELIVERY_API_TOKEN,
     });
 
-    const title = await contentfulClient.getEntry<EntrySkeleton>("5k6saMkIV7PHFhNDxmZiIG");
-    const body = await contentfulClient.getEntry<EntrySkeleton>("1VCVunA3sIV5Ch1qGHEKZZ");
+    const { titleEntry, bodyEntry } = await client.fetch<EntriesQuery>(printQuery(query));
 
     return {
-      title: title.fields.body,
-      body: body.fields.body,
+      title: titleEntry?.body?.json,
+      body: bodyEntry?.body?.json,
     };
   } else {
     return {
