c409cb9e3955f0271a022bd6e24e94c4111fe7e8
Benaiah Mischenko <benaiah@mischenko.com>
2023-06-07 14:38:36 -0700

    refresh the page on logout

--- src/lib/actions/logout.ts
+++ src/lib/actions/logout.ts
@@ -1,11 +1,25 @@
+import { page } from "$app/stores";
 import managementClient from "$lib/stores/managementClient";
 import userInfo from "$lib/stores/userInfo";
 import userToken from "$lib/stores/userToken";
+
+let refreshURL: string | undefined = undefined;
+
+page.subscribe(($page) => {
+  const newSearchParams = new URLSearchParams($page.url.searchParams);
+  newSearchParams.delete("preview");
+  refreshURL = `${$page.url.pathname}${newSearchParams.toString()}${$page.url.hash}`;
+});
 
 const logout = () => {
   userToken?.set(undefined);
   userInfo?.set(undefined);
   managementClient?.set(undefined);
+  if (refreshURL) {
+    window.location.href = refreshURL;
+  } else {
+    window.location.reload();
+  }
 };
 
 export default logout;
