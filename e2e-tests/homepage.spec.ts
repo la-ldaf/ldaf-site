7d4cad08bde5873ff499aa4c06c7abf88f203eb2
Benaiah Mischenko <benaiah@mischenko.com>
2023-06-07 16:42:40 -0700

    remove artificial failure

--- e2e-tests/homepage.spec.ts
+++ e2e-tests/homepage.spec.ts
@@ -1,7 +1,6 @@
 import { expect, test } from "@playwright/test";
 
 test("index page has expected h1", async ({ page }) => {
-  throw new Error("fail");
   await page.goto("/");
   await expect(page.getByRole("heading", { name: "Hello There." })).toBeVisible();
 });
