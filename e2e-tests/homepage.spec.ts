39a03eb7c1fab50ebfa2156fe23d0e613e559726
Benaiah Mischenko <benaiah@mischenko.com>
2023-06-07 16:19:50 -0700

    artificially fail e2e test

--- e2e-tests/homepage.spec.ts
+++ e2e-tests/homepage.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("index page has expected h1", async ({ page }) => {
+  throw new Error("fail");
   await page.goto("/");
   await expect(page.getByRole("heading", { name: "Hello There." })).toBeVisible();
 });
