d8e98a799be29d1b3d4640c6ff8038b05ea31b64
Benaiah Mischenko <benaiah@mischenko.com>
2023-06-07 14:41:02 -0700

    improve parseHashQuery

--- src/lib/util/parseHashQuery.ts
+++ src/lib/util/parseHashQuery.ts
@@ -1,7 +1,11 @@
-export default (hash: string): Record<string, string> =>
-  Object.fromEntries(
+export default (hash: string): Record<string, string | boolean> => {
+  const queryStart = hash.indexOf("?");
+  if (!queryStart) return {};
+  return Object.fromEntries(
     hash
-      .substring(1)
+      .substring(queryStart + 1)
       .split("&")
       .map((pair) => pair.split("="))
+      .map(([key, value]) => [key, value || true])
   );
+};
