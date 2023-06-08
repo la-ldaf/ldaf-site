18c88c5ae859f913f0cd905daad673aeef8c56f4
Benaiah Mischenko <benaiah@mischenko.com>
2023-06-07 17:06:38 -0700

    undo "fix" to parseHashQuery

--- src/lib/util/parseHashQuery.ts
+++ src/lib/util/parseHashQuery.ts
@@ -1,9 +1,7 @@
 export default (hash: string): Record<string, string> => {
-  const queryStart = hash.indexOf("?");
-  if (!queryStart) return {};
   return Object.fromEntries(
     hash
-      .substring(queryStart + 1)
+      .substring(1) // #
       .split("&")
       .map((pair) => pair.split("="))
       .map(([key, value]) => [key, value ?? ""])
