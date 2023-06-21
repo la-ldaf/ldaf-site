63661f598955c5c6953ab6688cc712eb5985874d
Louis Fettet <louis@adhocteam.us>
2023-06-21 11:15:34 -0500

    Move test content for Office Page into its own file.

--- src/routes/about/organization/[slug]/+page.server.ts
+++ src/routes/about/organization/[slug]/+page.server.ts
@@ -3,7 +3,7 @@ import gql from "graphql-tag";
 import { print as printQuery } from "graphql";
 
 import contentfulFetch, { contentfulConnected } from "$lib/services/contentful";
-import documentWithParagraphData from "$lib/components/ContentfulRichText/__tests__/documents";
+import OfficePageTestContent from "./__tests__/OfficePageTestContent";
 
 import type { PageServerLoad } from "./$types";
 import type { OfficePage } from "$lib/services/contentful/schema";
@@ -60,75 +60,16 @@ const query = gql`
 
 export const load = (async ({ params }): Promise<OfficePage> => {
   const { slug } = params;
-  if (contentfulConnected()) {
-    const data = await contentfulFetch<OfficePageQuery>(printQuery(query));
-    if (data) {
-      const officePages = data?.officePageCollection?.items as OfficePage[];
-      const matchedOfficePage = officePages.find(
-        (officePage) => officePage.metadata?.slug === slug
-      );
-      if (matchedOfficePage) {
-        return matchedOfficePage;
-      }
-    }
-    throw error(404);
-  } else {
-    return {
-      sys: { id: "0", environmentId: "", spaceId: "" },
-      contentfulMetadata: { tags: [] },
-      pageTitle: "Sample Office Page",
-      subheading:
-        "This page is loaded with test data since a connection with Contentful could not be established.",
-      description: {
-        json: documentWithParagraphData.documentWithParagraph.document,
-        links: {
-          assets: { block: [], hyperlink: [] },
-          entries: { block: [], hyperlink: [], inline: [] },
-        },
-      },
-      servicesAndPrograms: {
-        json: documentWithParagraphData.documentWithParagraph.document,
-        links: {
-          assets: { block: [], hyperlink: [] },
-          entries: { block: [], hyperlink: [], inline: [] },
-        },
-      },
-      mailingAddress: {
-        sys: {
-          id: "1",
-          environmentId: "",
-          spaceId: "",
-        },
-        contentfulMetadata: { tags: [] },
-        name: "Sample Office",
-        streetAddress1: "123 Main Street",
-        streetAddress2: "Suite 456",
-        city: "City",
-        state: "ST",
-        zip: "12345",
-      },
-      contactsCollection: {
-        limit: 100,
-        skip: 0,
-        total: 2,
-        items: [
-          {
-            sys: { id: "2", environmentId: "", spaceId: "" },
-            contentfulMetadata: { tags: [] },
-            entityName: "Contact Person 1",
-            phone: "(123) 456-7890",
-            phoneExt: "123",
-            email: "contact1@example.com",
-          },
-          {
-            sys: { id: "3", environmentId: "", spaceId: "" },
-            contentfulMetadata: { tags: [] },
-            entityName: "Contact Person 2",
-            phone: "(789) 012-3456",
-            email: "contact2@example.com",
-          },
-        ],
-      },
-    };
+  if (!contentfulConnected()) {
+    return OfficePageTestContent;
   }
+  const data = await contentfulFetch<OfficePageQuery>(printQuery(query));
+  if (data) {
+    const officePages = data?.officePageCollection?.items as OfficePage[];
+    const matchedOfficePage = officePages.find((officePage) => officePage.metadata?.slug === slug);
+    if (matchedOfficePage) {
+      return matchedOfficePage;
+    }
+  }
+  throw error(404);
 }) satisfies PageServerLoad;
