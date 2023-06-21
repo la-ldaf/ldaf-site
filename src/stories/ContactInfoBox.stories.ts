540733952873f18000a6d6633495fdcd58cc35e3
Louis Fettet <louis@adhocteam.us>
2023-06-21 11:36:33 -0500

    Use test content in ContactInfoBox story and link to Figma design file.

--- src/stories/ContactInfoBox.stories.ts
+++ src/stories/ContactInfoBox.stories.ts
@@ -2,34 +2,26 @@ import type { Meta, StoryObj } from "@storybook/svelte";
 
 import ContactInfoBox from "$lib/components/ContactInfoBox";
 
+import OfficePageTestContent from "../routes/about/organization/[slug]/__tests__/OfficePageTestContent";
+
 const meta = {
   title: "Components/ContactInfoBox",
   component: ContactInfoBox,
   tags: ["autodocs"],
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/oGKbyCnCRRdNzLYbiags93/LDAF-Component-Library-USWDS-3.0.2?type=design&node-id=2836-8572&t=S7mXyVT1E3IbZMMx-4",
+    },
+  },
 } satisfies Meta<ContactInfoBox>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// Default args can be found in the Link wrapper LinkView.svelte
 export const Default: Story = {
   args: {
-    address: {
-      name: "Sample Office",
-      streetAddress1: "123 Main Street",
-      streetAddress2: "Suite 456",
-      city: "City",
-      state: "ST",
-      zip: "12345",
-    },
-    contacts: [
-      {
-        sys: { id: "1" },
-        entityName: "Contact Person",
-        phone: "(123) 456-7890",
-        phoneExt: "123",
-        email: "contact@example.com",
-      },
-    ],
+    address: OfficePageTestContent.mailingAddress,
+    contacts: OfficePageTestContent.contactsCollection.items,
   },
 };
