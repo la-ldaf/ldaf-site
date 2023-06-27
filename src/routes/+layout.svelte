81a862670c4706d72c588b556465542b8bc3e2b8
Dan Hinze <hinzed1127@gmail.com>
2023-06-27 14:18:39 -0500

    Use  for footer styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -40,7 +40,7 @@
     <slot />
   </main>
   <!-- TODO: reinstate <Footer /> once designed and implemented -->
-  <footer class="usa-footer padding-top-7 padding-bottom-1 text-center">
+  <footer class="usa-footer ldaf-footer--main padding-top-7 padding-bottom-1 text-center">
     ©2023 Louisiana Department of Agriculture and Forestry. All rights reserved
   </footer>
   <!-- TODO: reinstate <Identifier /> once designed and implemented -->
