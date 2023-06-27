a69768030822f05a08b5fb189cd2f15a56f5eaeb
Dan Hinze <hinzed1127@gmail.com>
2023-06-27 17:26:57 -0500

    Wrap search page in <main> element

--- src/routes/search/+layout.svelte
+++ src/routes/search/+layout.svelte
@@ -1,3 +1,5 @@
-<div class="usa-section usa-prose grid-container">
-  <slot />
-</div>
+<main id="main-content">
+  <div class="usa-section usa-prose grid-container">
+    <slot />
+  </div>
+</main>
