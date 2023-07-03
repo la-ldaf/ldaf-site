803841c8103933c01376cd6597cde415e5429b42
Benaiah Mischenko <benaiah@mischenko.com>
2023-07-03 16:00:02 -0700

    remove console.log

--- src/lib/components/ContentfulRichText/context.ts
+++ src/lib/components/ContentfulRichText/context.ts
@@ -6,14 +6,10 @@ export const linksKey = Symbol("contentfulRichTextLinks");
 const getAssetLinksMap = (links: Links, key: "block" | "hyperlink") =>
   new Map(links.assets[key]?.map((link) => [link.sys.id, link]));
 
-export const createLinksContext = (links: Links): LinksContext => {
-  const blocksLinksMap = getAssetLinksMap(links, "block");
-  console.log({ links, blocksLinksMap: Object.fromEntries(blocksLinksMap.entries()) });
-  return {
-    links,
-    linksMaps: {
-      block: getAssetLinksMap(links, "block"),
-      hyperlink: getAssetLinksMap(links, "hyperlink"),
-    },
-  };
-};
+export const createLinksContext = (links: Links): LinksContext => ({
+  links,
+  linksMaps: {
+    block: getAssetLinksMap(links, "block"),
+    hyperlink: getAssetLinksMap(links, "hyperlink"),
+  },
+});
