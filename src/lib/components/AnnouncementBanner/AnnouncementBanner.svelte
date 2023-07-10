bc6f182e65556f5d650afba9ebcc3048077c7e15
Dan Hinze <hinzed1127@gmail.com>
2023-07-10 16:48:35 -0500

    Make aria-label a prop, rename icon class

--- src/lib/components/AnnouncementBanner/AnnouncementBanner.svelte
+++ src/lib/components/AnnouncementBanner/AnnouncementBanner.svelte
@@ -6,18 +6,19 @@
   import { url as arrowIcon } from "$icons/arrow_forward";
   import classNames from "$lib/util/classNames";
 
+  export let ariaLabel = "Top-level announcement";
   export let headerText = "Here's a site-wide announcement!";
   export let linkText = "Read more here";
   export let href = "/";
   let classes = classNames("usa-banner", `ldaf-banner--announcement`);
 </script>
 
-<section id="top" class={classes} aria-label="Official website of the Louisiana state government">
+<section id="top" class={classes} aria-label={ariaLabel}>
   <header class="usa-banner__header">
     <div class="usa-banner__inner">
       <div class="grid-col-auto">
         <img
-          class="usa-banner__header-flag ldaf-logo"
+          class="usa-banner__header-flag banner-icon"
           src={lightbulbIcon}
           alt="Lightbulb icon"
           width={16}
