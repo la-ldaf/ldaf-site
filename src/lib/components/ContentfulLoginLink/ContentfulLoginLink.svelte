c85fbd6840403f729e4fc1512f3d19d7c78678eb
Benaiah Mischenko <benaiah@mischenko.com>
2023-07-10 12:06:11 -0700

    simplify ContentfulLoginLink

--- src/lib/components/ContentfulLoginLink/ContentfulLoginLink.svelte
+++ src/lib/components/ContentfulLoginLink/ContentfulLoginLink.svelte
@@ -5,32 +5,27 @@
     PUBLIC_CONTENTFUL_OAUTH_CLIENT_ID,
     PUBLIC_CONTENTFUL_OAUTH_CLIENT_REDIRECT_URI,
   } from "$env/static/public";
-  import { browser } from "$app/environment";
   import { page } from "$app/stores";
 
   type $$Props = HTMLAnchorAttributes;
 
   const baseURL = PUBLIC_CONTENTFUL_OAUTH_ENDPOINT;
 
-  let loginParams: Record<string, string> = {},
-    loginLink = "";
+  const defaultLoginParams = {
+    response_type: "token",
+    scope: "content_management_read",
+    client_id: PUBLIC_CONTENTFUL_OAUTH_CLIENT_ID,
+    redirect_uri: encodeURIComponent(PUBLIC_CONTENTFUL_OAUTH_CLIENT_REDIRECT_URI),
+  };
 
-  if (browser) {
-    // we're using page.subscribe instead of just $page because of a weird error that only happens
-    // in tests
-    page.subscribe(($page) => {
-      loginParams = {
-        response_type: "token",
-        scope: "content_management_read",
-        client_id: PUBLIC_CONTENTFUL_OAUTH_CLIENT_ID,
-        redirect_uri: encodeURIComponent(PUBLIC_CONTENTFUL_OAUTH_CLIENT_REDIRECT_URI),
-        state: encodeURIComponent($page.url.pathname + $page.url.search + $page.url.hash),
-      };
-      loginLink = `${baseURL}?${Object.entries(loginParams)
-        .map(([key, value]) => `${key}=${value}`)
-        .join("&")}`;
-    });
-  }
+  $: loginParams = {
+    ...defaultLoginParams,
+    state: encodeURIComponent($page.url.pathname + $page.url.search + $page.url.hash),
+  };
+
+  $: loginLink = `${baseURL}?${Object.entries(loginParams)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&")}`;
 </script>
 
 <a class="login-link" href={loginLink} {...$$props}>
