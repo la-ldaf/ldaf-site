767eb0ed6ef46d313e85b2ed37171a33d49cd7e7
Louis Fettet <louis@adhocteam.us>
2023-07-14 12:08:27 -0500

    Sort SideNav by utilizing featured Core Content pages from Top Tier pages.

--- src/lib/components/SideNav/SideNav.server.ts
+++ src/lib/components/SideNav/SideNav.server.ts
@@ -9,38 +9,74 @@ const sideNavMap: SideNavMap = new Map();
 // recursive function that builds out a tree of children, grandchildren, etc.
 const buildSideNavTree = (
   pageMetadataMap: PageMetadataMap,
+  featuredPages: string[],
   metadata: PageMetadataMapItem
 ): SideNavItem[] => {
   const tree: SideNavItem[] = [];
-  if (metadata.children) {
-    metadata.children.forEach((childId) => {
-      const child = pageMetadataMap.get(childId);
-      if (child && child.sys && child.title && child.url) {
-        tree.push({
-          id: child.sys.id,
-          title: child.title,
-          link: child.url,
-          children: buildSideNavTree(pageMetadataMap, child),
-        });
+  const { children } = metadata;
+  if (children) {
+    featuredPages.forEach((featuredPageId) => {
+      const matchedChildId = children.find((childId) => childId === featuredPageId);
+      if (matchedChildId) {
+        const child = pageMetadataMap.get(matchedChildId);
+        if (child && child.sys && child.title && child.url) {
+          // push the child to the tree and build out its children
+          tree.push({
+            id: child.sys.id,
+            title: child.title,
+            link: child.url,
+            children: buildSideNavTree(pageMetadataMap, featuredPages, child),
+          });
+        }
       }
     });
+    // then add the rest in alphabetical order
+    const nonFeaturedPages: SideNavItem[] = [];
+    children.forEach((childId) => {
+      // skip if we've already added it
+      const childAlreadyInTree = tree.find((leaf) => leaf.id === childId);
+      if (!childAlreadyInTree) {
+        const child = pageMetadataMap.get(childId);
+        if (child && child.sys && child.title && child.url) {
+          nonFeaturedPages.push({
+            id: child.sys.id,
+            title: child.title,
+            link: child.url,
+            children: buildSideNavTree(pageMetadataMap, featuredPages, child),
+          });
+        }
+      }
+    });
+    nonFeaturedPages.sort(({ title: a }, { title: b }) => a.localeCompare(b));
+    tree.push(...nonFeaturedPages);
   }
-  return tree.sort(({ title: a }, { title: b }) => a.localeCompare(b));
+  return tree;
 };
 
 export const loadSideNavMap = async (
   pageMetadataMap: PageMetadataMap,
   mainMenu: NavMenuType[]
 ): Promise<SideNavMap> => {
-  // construct the full side navs for each top tier
+  // construct the full side navs for each top tier utilizing work done in the main menu
   mainMenu.forEach((topTier) => {
     const metadata = pageMetadataMap.get(topTier.id);
-    if (metadata && metadata.slug && metadata.sys && metadata.title && metadata.url) {
+    if (
+      metadata &&
+      metadata.slug &&
+      metadata.sys &&
+      metadata.title &&
+      metadata.url &&
+      topTier.children
+    ) {
+      // utilize the sort order from the main menu
+      const featuredPages = topTier.children.map((menuItem) => menuItem.id);
+      // remove the first item since it's always the top tier page
+      featuredPages.splice(0, 1);
       sideNavMap.set(metadata.slug, {
         id: metadata.sys.id,
         title: metadata.title,
         link: metadata.url,
-        children: buildSideNavTree(pageMetadataMap, metadata),
+        children: buildSideNavTree(pageMetadataMap, featuredPages, metadata),
       });
     }
   });
