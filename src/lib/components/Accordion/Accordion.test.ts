1da5ea00318c524a2aa6086ba75682cfea027174
Dan Hinze <hinzed1127@gmail.com>
2023-07-20 11:52:40 -0500

    Add tests and storybook for <Accordion />

---
+++ src/lib/components/Accordion/Accordion.test.ts
@@ -0,0 +1,61 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/svelte";
+import { describe, it, expect } from "vitest";
+
+// AccordionView functions similarly to CardView.svelte
+import AccordionView from "../../../stories/AccordionView.svelte";
+
+const defaultProps = {
+  accordionOneContent: "Content 1",
+  accordionTwoContent: "Content 2",
+  titleOne: "Title 1",
+  titleTwo: "Title 2",
+  multiselectable: false,
+};
+
+describe("Accordion", () => {
+  it("renders the default view", async () => {
+    render(AccordionView, { props: defaultProps });
+    expect(screen.getByText("Title 1")).toBeVisible();
+    expect(screen.getByText("Title 2")).toBeVisible();
+    expect(screen.getByText("Content 1")).not.toBeVisible();
+    expect(screen.getByText("Content 2")).not.toBeVisible();
+  });
+  it("Only displays AccordionItem contents one at a time by default", async () => {
+    render(AccordionView, { props: defaultProps });
+    // Initial Render
+    expect(screen.getByText("Content 1")).not.toBeVisible();
+    expect(screen.getByText("Content 2")).not.toBeVisible();
+
+    await screen.getByText("Title 1").click();
+    expect(screen.getByText("Content 1")).toBeVisible();
+    expect(screen.getByText("Content 2")).not.toBeVisible();
+
+    await screen.getByText("Title 2").click();
+    expect(screen.getByText("Content 1")).not.toBeVisible();
+    expect(screen.getByText("Content 2")).toBeVisible();
+  });
+  it("Displays multiple AccordionItems when multiselectable is true", async () => {
+    render(AccordionView, {
+      props: {
+        ...defaultProps,
+        multiselectable: true,
+      },
+    });
+    // Initial Render
+    expect(screen.getByText("Content 1")).not.toBeVisible();
+    expect(screen.getByText("Content 2")).not.toBeVisible();
+
+    await screen.getByText("Title 1").click();
+    expect(screen.getByText("Content 1")).toBeVisible();
+    expect(screen.getByText("Content 2")).not.toBeVisible();
+
+    await screen.getByText("Title 2").click();
+    expect(screen.getByText("Content 1")).toBeVisible();
+    expect(screen.getByText("Content 2")).toBeVisible();
+
+    await screen.getByText("Title 1").click();
+    expect(screen.getByText("Content 1")).not.toBeVisible();
+    expect(screen.getByText("Content 2")).toBeVisible();
+  });
+});
---
+++ src/stories/AccordionView.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Accordion, { AccordionItem } from "$lib/components/Accordion";
+
+  export let accordionOneContent = "Accordion 1 Content";
+  export let accordionTwoContent = "Accordion 2 Content";
+  export let titleOne = "Accordion 1 Title";
+  export let titleTwo = "Accordion 2 Title";
+  export let multiselectable = false;
+  export let bordered = true;
+
+  let className = "";
+  export { className as class };
+</script>
+
+<Accordion {multiselectable} {bordered} class={className}>
+  <AccordionItem title={titleOne}>
+    {accordionOneContent}
+  </AccordionItem>
+  <AccordionItem title={titleTwo}>
+    {accordionTwoContent}
+  </AccordionItem>
+</Accordion>
