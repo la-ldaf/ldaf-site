b3393f2aba30f1c6441f748ff41fbfd42d106454
Benaiah Mischenko <benaiah.mischenko@adhocteam.us>
2023-07-20 17:59:37 -0700

    Basic header image optimization (#278)

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
   import "./Header.scss";
-  import ldafLogo from "$lib/assets/ldaf-flat-logo-transparent.png";
   import { url as closeIcon } from "$icons/close";
-
-  import type { NavItemType, NavLinkType } from "./Nav";
-  import type { SiteTitleType } from "./Title";
+  import headerBg from "$lib/assets/header-background-optimized.jpg?quality=85&imagetools";
+  import headerBgBlurhash, {
+    width as headerBgWidth,
+    height as headerBgHeight,
+  } from "$lib/assets/header-background-optimized.jpg?blurhash";
+  import headerBgWebp from "$lib/assets/header-background-optimized.jpg?quality=85&format=webp&imagetools";
+  import headerBgAvif from "$lib/assets/header-background-optimized.jpg?quality=85&format=avif&imagetools";
 
   import classNames from "$lib/util/classNames";
   import Icon from "$lib/components/Icon";
   import Link from "$lib/components/Link";
   import Search from "$lib/components/Search";
-  import Title from "./Title";
-  import Nav from "./Nav";
+  import Image from "$lib/components/Image";
+  import Title, { type SiteTitleType } from "./Title";
+  import Nav, { type NavItemType, type NavLinkType } from "./Nav";
+  import Logo from "./Logo";
 
   export let navItems: NavItemType[] = [];
   export let siteTitle: SiteTitleType;
@@ -27,6 +32,20 @@
 <!-- TODO: Continue replacing parts of this file with components and content from the CMS. -->
 <!-- TODO: Possibly add support for other header variations, e.g. usa-header--basic -->
 <header class="ldaf-header usa-header usa-header--extended">
+  <Image
+    class="ldaf-header-bg-img"
+    src={headerBg}
+    sources={[
+      { type: "image/avif", srcset: [headerBgAvif] },
+      { type: "image/webp", srcset: [headerBgWebp] },
+      { type: "image/jpeg", srcset: [headerBg] },
+    ]}
+    blurhash={headerBgBlurhash}
+    alt=""
+    width={headerBgWidth}
+    height={headerBgHeight}
+    fit
+  />
   <div class="ldaf-nav usa-navbar">
     <Title {siteTitle} />
     <!--TODO: Replace with content from CMS. -->
@@ -41,8 +60,7 @@
       </button>
 
       <a class="ldaf-logo__compact" href="/">
-        <!-- TODO: Replace alt value with content from CMS. -->
-        <img src={ldafLogo} alt="Louisiana Department of Agriculture and Forestry Home" />
+        <Logo />
       </a>
       <div class="ldaf-commissioner__compact">
         <span>{siteTitle.commissionerRow1}</span>
