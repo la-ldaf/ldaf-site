cb76dfa55b156f179046eed8c4a547bf6dca9f84
Louis Fettet <louis@adhocteam.us>
2023-07-24 10:36:43 -0500

    LDAF-350 Set up foundation for future navigation and routing. (#273)

---
+++ src/routes/[topTierPage]/[...serviceGroupPage]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let data;
+  $: ({ pageMetadata } = data);
+</script>
+
+<div class="usa-section grid-container">
+  <main id="main-content">
+    <h1>{pageMetadata?.title}</h1>
+    <p>This is a stub for a Service Group page!</p>
+  </main>
+</div>
