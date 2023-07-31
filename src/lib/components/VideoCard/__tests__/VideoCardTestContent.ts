cd1c8e030c13b0197b25028c6f91014c23685c76
Louis Fettet <louis@adhocteam.us>
2023-07-31 17:38:35 -0500

    Update test data to match new prop names.

--- src/lib/components/VideoCard/__tests__/VideoCardTestContent.ts
+++ src/lib/components/VideoCard/__tests__/VideoCardTestContent.ts
@@ -1,6 +1,6 @@
 export default {
   url: "https://www.youtube.com/watch?v=3HRGzFjA_cU",
-  title: "Louisiana Department of Agriculture and Forestry",
-  description:
+  customTitle: "Louisiana Department of Agriculture and Forestry",
+  customDescription:
     "The agriculture and forestry sectors are the backbone of Louisiana’s economy and play a role in the lives of all Louisiana’s citizens.\n\nThat’s why we work so hard to fulfill our mission to promote, protect and advance agriculture, forestry and, soil and water resources of Louisiana through our programs and services.",
 };
