d1361b8e965303aa002c5a53dafd4f1de5a8f033
Louis Fettet <louis@adhocteam.us>
2023-08-02 16:48:23 -0500

    Wrap site title in H1 if on homepage, and fix e2e tests.

--- e2e-tests/homepage.spec.ts
+++ e2e-tests/homepage.spec.ts
@@ -2,5 +2,9 @@ import { expect, test } from "@playwright/test";
 
 test("index page has expected h1", async ({ page }) => {
   await page.goto("/");
-  await expect(page.getByRole("heading", { name: "Hello There." })).toBeVisible();
+  await expect(
+    page.getByRole("heading", {
+      name: "Department of Agriculture and Forestry\nState of Louisiana",
+    })
+  ).toBeVisible();
 });
---
+++ src/lib/components/ConditionalWrapper/ConditionalWrapper.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  // Based on https://svelte.dev/repl/983851f4fb7044e8b5d66a53ca0b356b?version=3.48.0
+  // When condition is met, wraps slot in the provided element.
+  export let condition = false;
+  export let tag = "div";
+</script>
+
+{#if condition && tag}
+  <svelte:element this={tag} {...$$restProps}><slot /></svelte:element>
+{:else}
+  <slot />
+{/if}
