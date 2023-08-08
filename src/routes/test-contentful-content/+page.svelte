8814b28c9f0bd99d16d438d835f2c49fa53bda1b
Benaiah Mischenko <benaiah.mischenko@adhocteam.us>
2023-08-08 13:41:14 -0700

    Remove unused pages (#362)

--- src/routes/test-contentful-content/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import ContentfulRichText from "$lib/components/ContentfulRichText";
-  export let data;
-</script>
-
-<ContentfulRichText document={data.document} links={data.links} blurhashes={data.blurhashes} />
