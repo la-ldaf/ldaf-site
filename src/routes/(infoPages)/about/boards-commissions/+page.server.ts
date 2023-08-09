1b44703f95ca60e24cf2a13df123b40d708f17ec
Louis Fettet <louis@adhocteam.us>
2023-08-09 14:56:39 -0500

    Remove unimplemented aggregate pages for offices and for boards and commissions. Remove unimplemented board or commission page.

--- src/routes/(infoPages)/about/boards-commissions/+page.server.ts
+++
@@ -1,9 +0,0 @@
-export const load = async ({ parent }) => {
-  const { pageMetadataMap } = await parent();
-  const matchedPageMetadata = [...pageMetadataMap].find(([_, page]) => {
-    return page.url === "/about/boards-commissions";
-  });
-  if (matchedPageMetadata && matchedPageMetadata.length === 2) {
-    return { pageMetadata: matchedPageMetadata[1] };
-  }
-};
--- src/routes/(infoPages)/about/organization/+page.server.ts
+++
@@ -1,9 +0,0 @@
-export const load = async ({ parent }) => {
-  const { pageMetadataMap } = await parent();
-  const matchedPageMetadata = [...pageMetadataMap].find(([_, page]) => {
-    return page.url === "/about/organization";
-  });
-  if (matchedPageMetadata && matchedPageMetadata.length === 2) {
-    return { pageMetadata: matchedPageMetadata[1] };
-  }
-};
