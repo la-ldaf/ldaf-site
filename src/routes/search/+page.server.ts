7e2b5e8657364d3317d888e3b8128c9ec7693916
Louis Fettet <louis@adhocteam.us>
2023-08-09 18:48:57 -0500

    Set up title tag and meta tags for all pages.

---
+++ src/routes/search/+page.server.ts
@@ -0,0 +1,8 @@
+// TODO: We might want to instead set up a `Page details` entry in Contentful
+export const load = () => ({
+  pageMetadata: {
+    metaTitle: "Search",
+    metaDescription: "Search the LDAF website.",
+    url: "/search",
+  },
+});
