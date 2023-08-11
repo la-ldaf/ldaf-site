7fc89a7a90c8cc85244fc508524ee98cb22437db
Benaiah Mischenko <benaiah@mischenko.com>
2023-08-11 12:11:52 -0700

    add figma link to Pagination stories

--- src/stories/Pagination.stories.ts
+++ src/stories/Pagination.stories.ts
@@ -5,6 +5,12 @@ const meta = {
   title: "Components/Pagination",
   component: Pagination,
   tags: ["autodocs"],
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/oGKbyCnCRRdNzLYbiags93/LDAF-Component-Library-USWDS-3.0.2?type=design&node-id=2196-23143&mode=design&t=B9VmU9EMW2KfFXKQ-4",
+    },
+  },
   argTypes: {
     currentPage: { type: "number" },
     totalPages: { type: "number" },
