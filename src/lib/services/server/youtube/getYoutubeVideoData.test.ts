3f5c9f0b4102c3baeeec4edb5072994f408c9306
Benaiah Mischenko <benaiah@mischenko.com>
2023-08-17 16:08:40 -0700

    npm run format

--- src/lib/services/server/youtube/getYoutubeVideoData.test.ts
+++ src/lib/services/server/youtube/getYoutubeVideoData.test.ts
@@ -10,7 +10,7 @@ vi.mock("$env/static/public", () => ({ PUBLIC_YOUTUBE_API_KEY: "d4e5f6" }));
 
 describe("YouTube Fetch", () => {
   it("calls YouTube API", async () => {
-    const fetch = vi.fn(async () => Promise.resolve({ ok: true, json: () => sampleData })) as Mock
+    const fetch = vi.fn(async () => Promise.resolve({ ok: true, json: () => sampleData })) as Mock;
     const snippet = await getYoutubeVideoData("a1b2c3", { fetch });
     expect(fetch).toHaveBeenCalledWith(
       `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=a1b2c3&key=d4e5f6`
