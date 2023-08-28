f2ba803266b87dd64f401643577c74ddffb2bccc
Louis Fettet <louis@adhocteam.us>
2023-08-28 18:29:04 -0500

    Add "From the Commissioner" section to the home page (round 2) (#421)
    Co-authored-by: Dan Hinze <hinzed1127@gmail.com>
    Co-authored-by: Benaiah Mischenko <benaiah@mischenko.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,20 +10,35 @@
   import Card from "$lib/components/Card";
   import Icon from "$lib/components/Icon";
   import Image from "$lib/components/Image";
+  import ContentfulRichText from "$lib/components/ContentfulRichText";
   import VideoCard from "$lib/components/VideoCard";
   import ResourceLinks from "$lib/components/ResourceLinks";
 
   export let data;
   $: ({
-    homePage: { heroVideo, featuredServices, popularResources },
+    homePage: {
+      heroVideo,
+      featuredServices,
+      popularResources,
+      commissionerGreeting,
+      commissionerByline,
+      commissionerHeadshot,
+      commissionerBackground,
+    },
   } = data);
 
   const getCardSettings = (
     index: number,
   ): { card: "full" | "half" | "third"; button: Variant; imageLoading: "lazy" | "eager" } => {
-    if (index === 0) return { card: "full", button: "primary", imageLoading: "eager" };
-    else if (index < 3) return { card: "half", button: "secondary", imageLoading: "eager" };
-    else return { card: "third", button: "outline", imageLoading: "lazy" };
+    switch (index) {
+      case 0:
+        return { card: "full", button: "primary", imageLoading: "eager" };
+      case 1:
+      case 2:
+        return { card: "half", button: "secondary", imageLoading: "lazy" };
+      default:
+        return { card: "third", button: "outline", imageLoading: "lazy" };
+    }
   };
 </script>
 
@@ -47,16 +62,21 @@
         {@const { card: cardSize, button: buttonVariant, imageLoading } = getCardSettings(index)}
         <!-- TODO: Can't conditionally render a named slot, but ideally we only declare Card once here. -->
         {#if item?.heroImage?.imageSource?.url}
+          {@const {
+            heroImage: {
+              imageSource: { url, title, blurhash, width, height },
+            },
+          } = item}
           <Card class={`usa-card--${cardSize}`}>
             <h2 class="usa-card__heading" slot="header">{item.title}</h2>
             <Image
               slot="image"
-              src={item.heroImage.imageSource.url}
-              alt={item.heroImage.imageSource.title || "Card image"}
+              src={url}
+              alt={title || "Card image"}
               sources={getSources}
-              blurhash={item.heroImage.imageSource?.blurhash ?? undefined}
-              width={item.heroImage.imageSource.width ?? undefined}
-              height={item.heroImage.imageSource.height ?? undefined}
+              {blurhash}
+              {width}
+              {height}
               sizeType="card"
               loading={imageLoading}
             />
@@ -86,7 +106,59 @@
     </ul>
   {/if}
   {#if popularResources && popularResources.length > 0}
-    <h2 class="ldaf-homepage__popular-resources-heading">Popular resources</h2>
-    <ResourceLinks links={popularResources} />
+    <div class="ldaf-homepage__popular-resources">
+      <h2 class="ldaf-homepage__popular-resources-heading">Popular resources</h2>
+      <ResourceLinks links={popularResources} />
+    </div>
+  {/if}
+  {#if commissionerGreeting?.json && commissionerByline && commissionerHeadshot?.linkedImage?.url}
+    <section class="greeting-wrapper" aria-label="Introduction">
+      <!-- This section can render without the background image, so this is optional. -->
+      {#if commissionerBackground?.linkedImage?.url}
+        {@const {
+          linkedImage: { url, blurhash, width, height },
+        } = commissionerBackground}
+        <Image
+          class="greeting-background"
+          src={url}
+          alt=""
+          {blurhash}
+          {width}
+          {height}
+          preserveAspectRatio={false}
+          canUpscaleImage
+          sources={getSources}
+          sizeType="col-12"
+          loading="lazy"
+        />
+      {/if}
+      <div class="grid-row grid-gap greeting-content">
+        <div class="desktop:grid-col-7 greeting-body">
+          <ContentfulRichText document={commissionerGreeting.json} />
+          <p class="text-right">{commissionerByline}</p>
+        </div>
+        <div class="desktop:grid-col-5 greeting-commissioner-portrait-wrapper">
+          {#if commissionerHeadshot?.linkedImage?.url}
+            {@const {
+              linkedImage: { url, height, width, blurhash },
+              altText,
+            } = commissionerHeadshot}
+            {@const diameter = width && height ? Math.min(width, height) : width ?? height}
+            <Image
+              class="greeting-commissioner-portrait"
+              src={url}
+              alt={altText ?? "Headshot of the commissioner"}
+              {blurhash}
+              height={diameter}
+              width={diameter}
+              preserveAspectRatio={false}
+              sizes="(max-width: 1023px) calc(125px - 6px), 350px"
+              sources={getSources}
+              loading="lazy"
+            />
+          {/if}
+        </div>
+      </div>
+    </section>
   {/if}
 </main>
