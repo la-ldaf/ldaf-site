19f8e95ed24493183200556bf8e558ac660c04d0
Louis Fettet <louis@adhocteam.us>
2023-09-07 12:55:16 -0500

    Utilize the PageMetadata entries for News and Event for metaTitle and metaDescription.

--- src/routes/(infoPages)/about/news/shared.server.ts
+++ src/routes/(infoPages)/about/news/shared.server.ts
@@ -13,6 +13,7 @@ import {
 import assetPropsFragment from "$lib/fragments/assetProps";
 import entryPropsFragment from "$lib/fragments/entryProps";
 
+const newsBasePath = "/about/news";
 const limit = 10;
 
 export const query = gql`
@@ -61,7 +62,7 @@ export const loadBaseBreadcrumbs = async ({
   const aboutPageMetadataID = pathsToIDs.get("/about");
   if (!aboutPageMetadataID) return [];
   const { breadcrumbs } = pageMetadataMap.get(aboutPageMetadataID) ?? {};
-  return [...(breadcrumbs ?? []), { title: "News", link: "/about/news" }];
+  return [...(breadcrumbs ?? []), { title: "News", link: newsBasePath }];
 };
 
 export const loadNewsPage = async ({
@@ -74,7 +75,7 @@ export const loadNewsPage = async ({
     const breadcrumbsPromise = loadBaseBreadcrumbs({ parent }).then((baseBreadcrumbs) => [
       ...baseBreadcrumbs,
       ...(pageNumber > 1
-        ? [{ title: `Page ${pageNumber}`, link: `/about/news/page/${pageNumber}` }]
+        ? [{ title: `Page ${pageNumber}`, link: `${newsBasePath}/page/${pageNumber}` }]
         : []),
     ]);
     if (!CONTENTFUL_SPACE_ID || !CONTENTFUL_DELIVERY_API_TOKEN) {
@@ -85,6 +86,11 @@ export const loadNewsPage = async ({
         breadcrumbs: breadcrumbsPromise,
       };
     }
+    const { pageMetadataMap, pathsToIDs } = await parent();
+    const pageMetadataID = pathsToIDs.get(newsBasePath);
+    if (!pageMetadataID) break fetchData;
+    const pageMetadata = pageMetadataMap.get(pageMetadataID);
+    if (!pageMetadata) break fetchData;
     const client = getContentfulClient({
       spaceID: CONTENTFUL_SPACE_ID,
       token: CONTENTFUL_DELIVERY_API_TOKEN,
@@ -100,7 +106,12 @@ export const loadNewsPage = async ({
     }
     return {
       pageMetadata: {
-        metaTitle: pageNumber <= 1 ? "News" : `News - page ${pageNumber}`,
+        ...pageMetadata,
+        metaTitle:
+          pageNumber <= 1
+            ? pageMetadata.metaTitle
+            : `${pageMetadata.metaTitle} - page ${pageNumber}`,
+        url: pageNumber <= 1 ? newsBasePath : `${newsBasePath}/page/${pageNumber}`,
         breadcrumbs: await breadcrumbsPromise,
       },
       currentPageNumber: pageNumber,
