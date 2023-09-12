a17e53fc28c482b286d7b7f5373195c3d3aa5955
Louis Fettet <louis@adhocteam.us>
2023-09-12 17:43:20 -0500

    Proof of concept "Licensing and permits" aggregation page powered by Contentful tags.

--- src/routes/licensing-permits/+page.server.ts
+++ src/routes/licensing-permits/+page.server.ts
@@ -1,9 +1,51 @@
-export const load = async ({ parent }) => {
-  const { pageMetadataMap } = await parent();
-  const matchedPageMetadata = [...pageMetadataMap].find(([_, page]) => {
-    return page.url === "/licensing-permits";
-  });
-  if (matchedPageMetadata && matchedPageMetadata.length === 2) {
-    return { pageMetadata: matchedPageMetadata[1] };
+import gql from "graphql-tag";
+import { print as printQuery } from "graphql";
+import { error } from "@sveltejs/kit";
+import { CONTENTFUL_SPACE_ID, CONTENTFUL_DELIVERY_API_TOKEN } from "$env/static/private";
+import getContentfulClient from "$lib/services/contentful";
+
+import type { TaggedPagesQuery } from "./$queries.generated";
+
+const query = gql`
+  query TaggedPages {
+    pageMetadataCollection(
+      where: { contentfulMetadata: { tags: { id_contains_some: "licensingAndPermits" } } }
+      order: [title_ASC]
+    ) {
+      items {
+        sys {
+          id
+        }
+        title
+      }
+    }
   }
+`;
+
+export const load = async ({ parent }) => {
+  const { pageMetadataMap, pathsToIDs } = await parent();
+  fetchData: {
+    const metadataID = pathsToIDs.get(`/licensing-permits`);
+    if (!metadataID) break fetchData;
+    const pageMetadata = pageMetadataMap.get(metadataID);
+    if (!pageMetadata) break fetchData;
+    const client = getContentfulClient({
+      spaceID: CONTENTFUL_SPACE_ID,
+      token: CONTENTFUL_DELIVERY_API_TOKEN,
+    });
+    const data = await client.fetch<TaggedPagesQuery>(printQuery(query));
+    if (!data?.pageMetadataCollection?.items) break fetchData;
+    const taggedPages = data.pageMetadataCollection.items.map((taggedPage) => {
+      if (taggedPage?.sys.id) {
+        return pageMetadataMap.get(taggedPage?.sys.id);
+      }
+    });
+
+    return {
+      aggregationPage: {},
+      pageMetadata,
+      taggedPages,
+    };
+  }
+  throw error(404);
 };
--- src/routes/licensing-permits/+page.svelte
+++ src/routes/licensing-permits/+page.svelte
@@ -1,43 +1,18 @@
 <script lang="ts">
   import Link from "$lib/components/Link";
   export let data;
-  $: ({ pageMetadata } = data);
+  $: ({ pageMetadata, taggedPages } = data);
 </script>
 
 <div class="grid-container">
   <main class="margin-top-2 usa-prose" id="main-content">
     <h1>{pageMetadata?.title}</h1>
     <ul>
-      <li><Link href="/land/arborists">Arborist licensing</Link></li>
-      <li>
-        <Link href="/business/warehousing-distribution">Commodity warehousing and distribution</Link
-        >
-      </li>
-      <li>
-        <Link href="/plants/florists">Florist and flower dealer licensing</Link>
-      </li>
-      <li>
-        <Link href="/plants/industrial-hemp/hemp-licensing">Industrial hemp licensing</Link>
-      </li>
-      <li>
-        <Link href="/plants/nurseries-landscaping">Nursery and landscape licensing</Link>
-      </li>
-      <li>
-        <Link href="/plants/pesticides">Pesticide licensing and registration</Link>
-      </li>
-      <li>
-        <Link href="/plants/seeds/seed-dealers">Seed dealer licensing</Link>
-      </li>
-      <li>
-        <Link href="/business/pest-control/pest-control-licensing"
-          >Structural pest control licensing and registration</Link
-        >
-      </li>
-      <li>
-        <Link href="/business/weights-measures/weighmaster-technician-licensing"
-          >Weighmaster and technician licensing</Link
-        >
-      </li>
+      {#each taggedPages as page (page?.sys.id)}
+        {#if page?.url && page?.title}
+          <li><Link href={page.url}>{page.title}</Link></li>
+        {/if}
+      {/each}
     </ul>
   </main>
 </div>
