c1c2c0370b703232713e2b825c0335a9f67a37b9
Dan Hinze <hinzed1127@gmail.com>
2023-09-14 17:51:28 -0500

    Generate hash links for accordions, auto-expand

--- src/lib/components/Accordion/AccordionItem.svelte
+++ src/lib/components/Accordion/AccordionItem.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+  import { page } from "$app/stores";
+  import "./AccordionItem.scss";
   import { getAccordionContext } from "./context";
+  import Link from "$lib/components/Link";
+  import Icon from "$lib/components/Icon";
+  import { url as linkIcon } from "$icons/link";
 
   export let id: string;
   export let title: string | null | undefined = "Title";
@@ -11,9 +16,25 @@
 
   let expanded: boolean;
   $: expanded = $expandedItems[id] ?? false;
+
+  // strip out everything except characters, digits,
+  // and whitespace, then convert to Kebab case.
+  // E.g. "Title 2: This time it's personal"
+  // becomes "title-2-this-time-its-personal"
+  const titleHref = title
+    ? title
+        .replace(/[^A-Za-z0-9\s]/g, "")
+        .toLowerCase()
+        .split(" ")
+        .join("-")
+    : null;
+
+  $: if ($page.url.hash === `#${titleHref}` && $expandedItems) {
+    $expandedItems[id] = true;
+  }
 </script>
 
-<h3 {...$$restProps} class="usa-accordion__heading">
+<h3 {...$$restProps} class="usa-accordion__heading" id={titleHref}>
   <button
     bind:this={ref}
     type="button"
@@ -32,5 +53,8 @@
   </button>
 </h3>
 <div class="usa-accordion__content usa-prose" {id} hidden={!expanded}>
+  <Link class="accordion-deeplink" href={`#${titleHref}`}
+    >Link to item <Icon src={linkIcon} title="Link to item" /></Link
+  >
   <slot />
 </div>
