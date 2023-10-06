c3b7838cbd0e7fc25aa0d995dd34ecc9c46b09cb
Dan Hinze <hinzed1127@gmail.com>
2023-10-06 11:14:36 -0500

    Add deep links to accordion, auto-expand on page render (#461)

--- src/lib/util/slugify.ts
+++ src/lib/util/slugify.ts
@@ -1,8 +1,14 @@
-// very basic slugify, currently only in use in tests
+// Function goals:
+// - replace ampersands with "and" (preserve url
+// legibility without breaking routing)
+// - strip out everything except characters, digits,
+// and whitespace, then convert to Kebab case.
+// - E.g. "Title 2: This time it's personal"
+// becomes "title-2-this-time-its-personal"
 export default (s: string) =>
   s
     .toLowerCase()
-    .replaceAll(/[^a-z\s-_]/g, "")
+    .replaceAll(/&/g, "and")
+    .replaceAll(/[^a-z0-9\s-_]/g, "")
     .trim()
-    .replaceAll(/\s+/g, " ")
-    .replaceAll(" ", "-");
+    .replaceAll(/\s+/g, "-");
