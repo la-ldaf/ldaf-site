c5cffa8de017cb776ec2f9a1a2ca29c35706d0c5
Benaiah Mischenko <benaiah@mischenko.com>
2023-10-20 16:21:05 -0700

    add logout page

---
+++ src/routes/logout/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { enhance } from "$app/forms";
+  import { getCurrentUserStore } from "$lib/context/currentUser";
+
+  export let form;
+
+  const currentUser = getCurrentUserStore();
+
+  $: if (form?.success) currentUser.set(undefined);
+</script>
+
+<div class="grid-container">
+  <h1>Logout</h1>
+  {#if $currentUser}
+    {#if form?.success}
+      <p>Successfully logged out!</p>
+    {:else}
+      <form method="POST" use:enhance>
+        <input type="submit" value="Logout" />
+      </form>
+    {/if}
+  {:else}
+    <p>You are not logged in!</p>
+  {/if}
+</div>
