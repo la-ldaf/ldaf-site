13e4b8246c2288d7eec4053e12a63ea7bfe0b440
Benaiah Mischenko <benaiah@mischenko.com>
2023-10-20 14:45:15 -0700

    add handleCurrentUser hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,5 +3,11 @@ import { sequence } from "@sveltejs/kit/hooks";
 import handleSetupKVClient from "./hooks/server/handleSetupKVClient";
 import handleSetupContentfulClient from "./hooks/server/handleSetupContentfulClient";
 import handlePreload from "./hooks/server/handlePreload";
+import handleCurrentUser from "./hooks/server/handleCurrentUser";
 
-export const handle = sequence(handleSetupKVClient, handleSetupContentfulClient, handlePreload);
+export const handle = sequence(
+  handleSetupKVClient,
+  handleCurrentUser,
+  handleSetupContentfulClient,
+  handlePreload,
+);
