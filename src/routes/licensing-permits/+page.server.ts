bb27ee11b8fe7717829005fa33aee20813041a60
Louis Fettet <louis@adhocteam.us>
2023-10-24 20:41:33 -0500

    [LDAF-376] Use Contentful tags to populate "Licensing and permits" page (#456)
    Co-authored-by: Benaiah Mischenko <benaiah@mischenko.com>

--- src/routes/licensing-permits/+page.server.ts
+++ src/routes/licensing-permits/+page.server.ts
@@ -1,9 +1,173 @@
-export const load = async ({ parent }) => {
-  const { pageMetadataMap } = await parent();
-  const matchedPageMetadata = [...pageMetadataMap].find(([_, page]) => {
-    return page.url === "/licensing-permits";
-  });
-  if (matchedPageMetadata && matchedPageMetadata.length === 2) {
-    return { pageMetadata: matchedPageMetadata[1] };
+import gql from "graphql-tag";
+import { print as printQuery } from "graphql";
+import { error } from "@sveltejs/kit";
+import slugify from "$lib/util/slugify";
+
+import assetProps from "$lib/fragments/assetProps";
+import entryProps from "$lib/fragments/entryProps";
+
+import type { AggregationPageQuery, TaggedServicesQuery } from "./$queries.generated";
+
+type TaggedService =
+  | {
+      id: string;
+      title: string;
+      url: string;
+    }
+  | undefined;
+
+const aggregationPageQuery = gql`
+  # eslint-disable @graphql-eslint/selection-set-depth
+  ${assetProps}
+  ${entryProps}
+  query AggregationPage($metadataID: String!) {
+    aggregationCollection(where: { pageMetadata: { sys: { id: $metadataID } } }, limit: 1) {
+      items {
+        contentfulMetadata {
+          tags {
+            id
+          }
+        }
+        title
+        subhead
+        body {
+          json
+          links {
+            assets {
+              hyperlink {
+                ...AssetProps
+              }
+            }
+            entries {
+              hyperlink {
+                ...EntryProps
+              }
+            }
+          }
+        }
+        pageMetadata {
+          sys {
+            id
+          }
+        }
+      }
+    }
   }
+`;
+
+const taggedServicesQuery = gql`
+  query TaggedServices($aggregationTag: String!) {
+    serviceEntryCollection(
+      where: { contentfulMetadata: { tags: { id_contains_some: [$aggregationTag] } } }
+      order: [entryTitle_ASC]
+    ) {
+      items {
+        sys {
+          id
+        }
+        entryTitle
+        linkedFrom {
+          serviceGroupCollection(limit: 1) {
+            items {
+              pageMetadata {
+                sys {
+                  id
+                }
+              }
+              subheading
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const load = async ({ parent, locals: { contentfulClient } }) => {
+  fetchData: {
+    if (!contentfulClient) break fetchData;
+    const { pageMetadataMap, pathsToIDs } = await parent();
+    const metadataID = pathsToIDs.get(`/licensing-permits`);
+    if (!metadataID) break fetchData;
+    const pageMetadata = pageMetadataMap.get(metadataID);
+    if (!pageMetadata) break fetchData;
+    // Fetch content for the page as well as the tag used for aggregation
+    const aggregationPageData = await contentfulClient.fetch<AggregationPageQuery>(
+      printQuery(aggregationPageQuery),
+      {
+        variables: { metadataID },
+      },
+    );
+    if (!aggregationPageData?.aggregationCollection?.items) break fetchData;
+    const [aggregationPage] = aggregationPageData.aggregationCollection.items;
+    const aggregationPageTags =
+      aggregationPage?.contentfulMetadata?.tags?.map((tag) => tag?.id) ?? [];
+    const aggregationTag = aggregationPageTags.find((tag) => tag?.startsWith("type-"));
+    if (!aggregationTag) break fetchData;
+
+    // Use the aggregation tag to grab all the relevant services and their
+    //   parent pages.
+    const taggedServicesData = await contentfulClient.fetch<TaggedServicesQuery>(
+      printQuery(taggedServicesQuery),
+      { variables: { aggregationTag } },
+    );
+    if (!taggedServicesData?.serviceEntryCollection?.items) break fetchData;
+
+    // Create a map where the key is the page metadata ID for a core content
+    //   page, and the value is an object containing page information and the
+    //   services that appear on that page.
+    const serviceGroupMap = new Map<
+      string,
+      {
+        id: string;
+        title: string;
+        subheading: string | null | undefined;
+        url: string | null | undefined;
+        services: TaggedService[];
+      }
+    >();
+
+    taggedServicesData.serviceEntryCollection.items.forEach((taggedService) => {
+      // Get information about the core content entry that houses the service.
+      // Every service should appear on only one page.
+      const [serviceGroupEntry] = taggedService?.linkedFrom?.serviceGroupCollection?.items ?? [];
+      const pageMetadataID = serviceGroupEntry?.pageMetadata?.sys?.id;
+      // To construct the URL to the service accordion, we'll need the service
+      //   title and the core content page URL.
+      if (!taggedService?.entryTitle || !pageMetadataID) return;
+      // To render everything on the page, we'll also need the page metadata
+      //   title. The core content page subheading is optional.
+      const pageMetadataForServiceGroup = pageMetadataMap.get(pageMetadataID);
+      if (!pageMetadataForServiceGroup?.title) return;
+      // Construct the URL for the service accordion.
+      const url = `${pageMetadataForServiceGroup.url}#${slugify(taggedService.entryTitle)}`;
+      // Add the service group to the map if it doesn't already exist, then
+      //   add the service under it.
+      const serviceGroupMapEntry = serviceGroupMap.get(pageMetadataID) ?? {
+        id: pageMetadataID,
+        title: pageMetadataForServiceGroup.title,
+        subheading: serviceGroupEntry.subheading,
+        url: pageMetadataForServiceGroup.url,
+        services: [],
+      };
+      serviceGroupMapEntry.services.push({
+        id: taggedService?.sys.id,
+        title: taggedService?.entryTitle,
+        url,
+      });
+      serviceGroupMap.set(pageMetadataID, serviceGroupMapEntry);
+    });
+
+    // Convert our map into an array and sort it alphabetically by title.
+    const serviceGroups = Array.from(serviceGroupMap, ([_, serviceGroup]) => serviceGroup).sort(
+      (a, b) => (a.title > b.title ? 1 : -1),
+    );
+
+    return {
+      pageMetadata,
+      aggregationPage,
+      serviceGroups,
+    };
+  }
+  throw error(404);
 };
--- src/routes/licensing-permits/+page.svelte
+++ src/routes/licensing-permits/+page.svelte
@@ -1,43 +1,37 @@
 <script lang="ts">
+  import Breadcrumbs from "$lib/components/Breadcrumbs";
+  import ContentfulRichText from "$lib/components/ContentfulRichText";
   import Link from "$lib/components/Link";
   export let data;
-  $: ({ pageMetadata } = data);
+  $: ({ pageMetadata, aggregationPage, serviceGroups } = data);
+  $: breadcrumbs = pageMetadata?.breadcrumbs ?? [];
 </script>
 
-<div class="grid-container">
-  <main class="margin-top-2 usa-prose" id="main-content">
-    <h1>{pageMetadata?.title}</h1>
-    <ul>
-      <li><Link href="/land/arborists">Arborist licensing</Link></li>
-      <li>
-        <Link href="/business/warehousing-distribution">Commodity warehousing and distribution</Link
-        >
-      </li>
-      <li>
-        <Link href="/plants/florists">Florist and flower dealer licensing</Link>
-      </li>
-      <li>
-        <Link href="/plants/industrial-hemp/hemp-licensing">Industrial hemp licensing</Link>
-      </li>
-      <li>
-        <Link href="/plants/nurseries-landscaping">Nursery and landscape licensing</Link>
-      </li>
-      <li>
-        <Link href="/plants/pesticides">Pesticide licensing and registration</Link>
-      </li>
-      <li>
-        <Link href="/plants/seeds/seed-dealers">Seed dealer licensing</Link>
-      </li>
-      <li>
-        <Link href="/business/pest-control/pest-control-licensing"
-          >Structural pest control licensing and registration</Link
-        >
-      </li>
-      <li>
-        <Link href="/business/weights-measures/weighmaster-technician-licensing"
-          >Weighmaster and technician licensing</Link
-        >
-      </li>
-    </ul>
+<div class="grid-container margin-bottom-2">
+  <Breadcrumbs path={breadcrumbs} />
+  <main class="usa-prose" id="main-content">
+    <h1>{aggregationPage?.title}</h1>
+    {#if aggregationPage?.subhead}
+      <p class="usa-intro">{aggregationPage.subhead}</p>
+    {/if}
+    {#if aggregationPage?.body}
+      <ContentfulRichText document={aggregationPage.body.json} links={aggregationPage.body.links} />
+    {/if}
+    {#if serviceGroups}
+      {#each serviceGroups as serviceGroup (serviceGroup.id)}
+        <h2>{serviceGroup.title}</h2>
+        {#if serviceGroup?.subheading}
+          <p>{serviceGroup.subheading}</p>
+        {/if}
+        <ul>
+          <li><Link href={serviceGroup.url}>About {serviceGroup.title}</Link></li>
+          {#each serviceGroup.services as service (service?.id)}
+            <li>
+              <Link href={service?.url}>{service?.title}</Link>
+            </li>
+          {/each}
+        </ul>
+      {/each}
+    {/if}
   </main>
 </div>
