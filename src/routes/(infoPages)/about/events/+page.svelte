53540b6e8b8fe35d6582013dc7c8aa8a531b5e2c
Dan Hinze <hinzed1127@gmail.com>
2023-11-09 01:30:23 -0600

    Show an empty state for events, not the 404 page, when events.length is 0 (#514)

--- src/routes/(infoPages)/about/events/+page.svelte
+++ src/routes/(infoPages)/about/events/+page.svelte
@@ -6,7 +6,11 @@
   $: ({ events, currentPageNumber, totalPages } = data);
 </script>
 
-<h1>All events</h1>
+<h1>Upcoming events</h1>
+
+{#if events.length === 0}
+  <p>There are no upcoming events to show at this time.</p>
+{/if}
 
 <!-- See src/routes/(infoPages)/layout.scss for styling. -->
 <div class="ldaf-events-list-container">
