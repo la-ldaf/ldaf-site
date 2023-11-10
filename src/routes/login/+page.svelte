985fcf890259877f847eab579e74ea8a6ebd21f7
Benaiah Mischenko <benaiah@mischenko.com>
2023-11-10 15:51:33 -0800

    redirect to the previous page from login success

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,32 +1,49 @@
 <script lang="ts">
   import type { SubmitFunction } from "@sveltejs/kit";
   import { applyAction, enhance } from "$app/forms";
+  import { page } from "$app/stores";
+  import { browser } from "$app/environment";
+  import { goto } from "$app/navigation";
   import { getCurrentUserStore } from "$lib/context/currentUser";
+  import timeout from "$lib/util/timeout";
 
   export let form;
   const currentUser = getCurrentUserStore();
 
   const enhanceLogoutForm: SubmitFunction<{ success: boolean }> =
     () =>
-    async ({ result, update }) => {
-      if (result.type === "success" && result.data?.success) {
-        await applyAction({
-          type: "success",
-          status: result.status,
-          data: { success: true, currentUser: undefined },
-        });
-        currentUser.set(undefined);
-      }
-      await update();
-    };
+      async ({ result, update }) => {
+        if (result.type === "success" && result.data?.success) {
+          await applyAction({
+            type: "success",
+            status: result.status,
+            data: { success: true, currentUser: undefined },
+          });
+          currentUser.set(undefined);
+        }
+        await update();
+      };
 
   $: if (form?.success) currentUser.set(form?.currentUser);
+
+  let redirectPromise: Promise<void> | undefined;
+  $: redirectURL = $page.url.searchParams.get("state");
+  $: if (form?.success && form?.currentUser) currentUser.set(form.currentUser);
+  $: if (browser && form?.success && redirectURL && !redirectPromise) {
+    redirectPromise = (async () => {
+      await timeout(500);
+      await goto(redirectURL, { replaceState: true, invalidateAll: true });
+    })();
+  }
 </script>
 
 <div class="grid-container">
   {#if form?.success}
     {#if form.currentUser}
       <p>Successfully logged in!</p>
+      {#if redirectURL}
+        <p>Please wait to be redirected to the previous page.</p>
+      {/if}
     {:else}
       <p>Successfully logged out!</p>
     {/if}
