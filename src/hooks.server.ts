fafba5733951331ef91785856a23d02ccb4506e6
Louis Fettet <louis@adhocteam.us>
2024-07-09 11:24:59 -0500

    Block bad actors by immediately responding with an HTTP error code before performing any load logic.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,12 @@
 import { sequence } from "@sveltejs/kit/hooks";
 
-import handleSetupKVClient from "./hooks/server/handleSetupKVClient";
+import handleBadActors from "./hooks/server/handleBadActors";
 import handlePreload from "./hooks/server/handlePreload";
+import handleSetupKVClient from "./hooks/server/handleSetupKVClient";
 import handleCurrentUserAndContentfulClient from "./hooks/server/handleCurrentUserAndContentfulClient";
 
 export const handle = sequence(
+  handleBadActors,
   handlePreload,
   handleSetupKVClient,
   handleCurrentUserAndContentfulClient,
