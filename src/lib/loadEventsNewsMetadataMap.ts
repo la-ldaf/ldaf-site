1b09726bccb0699549d3d1adab5a2c244b1cfcda
Larsen Maxwell <lauren.maxwell@adhocteam.us>
2024-10-18 10:30:30 -0700

    Prettier.

--- src/lib/loadEventsNewsMetadataMap.ts
+++ src/lib/loadEventsNewsMetadataMap.ts
@@ -3,12 +3,9 @@ import { print as printQuery } from "graphql";
 import type { ContentfulClient } from "$lib/services/server/contentful";
 import type { MetadataMapItem } from "./types";
 
-const queryNewsAndEvents =  gql`
+const queryNewsAndEvents = gql`
   query newsCollection {
-    newsCollection(
-      where: { indexInSearch: true }
-      order: [publicationDate_DESC]
-    ) {
+    newsCollection(where: { indexInSearch: true }, order: [publicationDate_DESC]) {
       items {
         sys {
           id
@@ -21,10 +18,7 @@ const queryNewsAndEvents =  gql`
         byline
       }
     }
-    eventEntryCollection(
-      where: { indexInSearch: true }
-      order: [eventDateAndTime_ASC]
-    ) {
+    eventEntryCollection(where: { indexInSearch: true }, order: [eventDateAndTime_ASC]) {
       items {
         sys {
           id
@@ -47,60 +41,60 @@ type NewsAndEventsCollectionQuery = {
       title?: string | null;
       publicationDate?: string | null;
       slug?: string | null;
-    }[]
-  }
+    }[];
+  };
   eventEntryCollection: {
     items: {
       sys: {
-        id: string
-      }
+        id: string;
+      };
       __typename?: "News";
       title?: string | null;
       slug?: string | null;
       eventDateAndTime?: string | null;
       eventDescription?: string | null;
-    }[]
-  }
-}
+    }[];
+  };
+};
 
 // TODO: remove
 const constructEventSlug = (date: Date, slug: string): string =>
   `${date.toISOString().split("T")[0]}-${slug}`;
 
-export const loadEventsAndNewsMap = async (
-  {contentfulClient}: {contentfulClient?: ContentfulClient;}
-): Promise<Map<string, MetadataMapItem>> => {
-
+export const loadEventsAndNewsMap = async ({
+  contentfulClient,
+}: {
+  contentfulClient?: ContentfulClient;
+}): Promise<Map<string, MetadataMapItem>> => {
   const entryMap: NonNullable<Map<string, MetadataMapItem>> = new Map();
 
   if (!contentfulClient) return entryMap;
 
   const data = await contentfulClient.fetch<NewsAndEventsCollectionQuery>(
-    printQuery(
-      queryNewsAndEvents,
-    )
+    printQuery(queryNewsAndEvents),
   );
 
   if (data?.newsCollection?.items && data?.newsCollection?.items.length > 0) {
     const allNewsMetadata = data.newsCollection.items;
-    allNewsMetadata.forEach(item => {
+    allNewsMetadata.forEach((item) => {
       if (!item) return;
       const url = "about/news/article/" + item.slug;
-      entryMap.set(item.sys.id, {...item, url})
+      entryMap.set(item.sys.id, { ...item, url });
     });
   }
   if (data?.eventEntryCollection?.items && data?.eventEntryCollection?.items.length > 0) {
     const allEventMetadata = data.eventEntryCollection.items;
 
-    allEventMetadata.forEach(item => {
+    allEventMetadata.forEach((item) => {
       if (!item || !item.eventDateAndTime || !item.slug) return;
       // if event date has changed, the URL should update as well
-      const url = item.eventDateAndTime && item.slug 
-        ? `/about/events/event/${constructEventSlug(new Date(item.eventDateAndTime), item.slug)}`
-        : null;
-      entryMap.set(item.sys.id, {...item, url})
+      const url =
+        item.eventDateAndTime && item.slug
+          ? `/about/events/event/${constructEventSlug(new Date(item.eventDateAndTime), item.slug)}`
+          : null;
+      entryMap.set(item.sys.id, { ...item, url });
     });
   }
 
   return entryMap;
-}
+};
