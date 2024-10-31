a7e4cfa308e52eab97f1baba0e31c072d8d85abf
Louis Fettet <louis@adhocteam.us>
2024-10-30 20:24:59 -0500

    [Issue #580] Fix end-to-end tests.
    Signed-off-by: Louis Fettet <louis@adhocteam.us>

--- e2e-tests/homepage.spec.ts
+++ e2e-tests/homepage.spec.ts
@@ -4,7 +4,7 @@ test("index page has expected h1", async ({ page }) => {
   await page.goto("/");
   await expect(
     page.getByRole("heading", {
-      name: "Department of Agriculture and Forestry\nState of Louisiana",
+      name: "Louisiana Department of Agriculture and Forestry",
     }),
   ).toBeVisible();
 });
